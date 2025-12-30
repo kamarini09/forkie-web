94e6e87d584d94b3b6ae9b96849d3b4fdce141cb
Kamarini Moragianni <moragiannikam@gmail.com>
2025-12-30 21:53:34 +0100

    feat: create recipe page

--- src/app/(app)/recipes/create/page.tsx
+++ src/app/(app)/recipes/create/page.tsx
@@ -1,5 +1,10 @@
 import { RecipeForm } from "@/app/components/recipes/form/RecipeForm";
+import { FormPageShell } from "@/app/components/recipes/form/FormPageShell";
 
-export default function Page() {
-  return <RecipeForm />;
+export default function CreateRecipePage() {
+  return (
+    <FormPageShell title="Create recipe">
+      <RecipeForm mode="create" />
+    </FormPageShell>
+  );
 }
